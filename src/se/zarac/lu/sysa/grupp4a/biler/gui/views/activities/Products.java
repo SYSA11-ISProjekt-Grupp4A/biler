36781a2c8599206c73117f922721f9d7ae3c7107
Hannes Landstedt <github.com@zarac.se>
2014-01-13 15:29:14 +0100

    GUI : Products activity implemented.

--- src/se/zarac/lu/sysa/grupp4a/biler/gui/views/activities/Products.java
+++ src/se/zarac/lu/sysa/grupp4a/biler/gui/views/activities/Products.java
@@ -1,39 +1,22 @@
 package se.zarac.lu.sysa.grupp4a.biler.gui.views.activities;
 
-
-
+import java.util.Iterator;
+import se.zarac.lu.sysa.grupp4a.biler.Model;
+import se.zarac.lu.sysa.grupp4a.biler.gui.Button;
 import se.zarac.lu.sysa.grupp4a.biler.gui.GUI;
 import se.zarac.lu.sysa.grupp4a.biler.gui.View;
-import se.zarac.lu.sysa.grupp4a.biler.gui.styles.handson.JButton;
-
-
+import se.zarac.lu.sysa.grupp4a.biler.models.Product;
 
 @SuppressWarnings("serial")
 public class Products extends View {
- 
-  
-  public Products(GUI gui) {
+  public Products(final GUI gui) {
     super(gui);
+    add(new Button("Add Product") {
+      public void click() {
+        Product product = new Product("Pro yeah");
+        biler.add(product);
+        gui.view(product); } } );
     
-    add(new JButton("Bilar"));
-    add(new JButton("Sl�p"));
-    add(new JButton("�vrigt"));}
-    
-    
-    
- 
-  }
-
-
-
-
-  
-  
-  
- 
-    
-   
-  
-  
- 
-
+    Iterator<Model> i = biler.getIndex(Product.class).values().iterator();
+    while (i.hasNext()) {
+      add(gui.createView((Product)i.next(), GUI.ViewTypes.Short)); } } }
